62114ef37daf5c0d3ee8b3904b978ca12b6bc941
Zehan <zehan.kareem@gmail.com>
2026-01-23 20:11:05 +0600

    fix(iamzehan): (auth) ensure guest middleware applied to login and registration routes

--- Backend/API/src/routes/auth.routes.ts
+++ Backend/API/src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controller.js";
+import { ensureGuest } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", ensureGuest, register);
+router.post("/login", ensureGuest, login);
 
 export default router;
