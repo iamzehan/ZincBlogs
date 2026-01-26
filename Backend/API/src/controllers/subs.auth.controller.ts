55dd72c08f1bd1c138104f181c0213a52ee74f9f
Zehan <zehan.kareem@gmail.com>
2026-01-26 23:49:09 +0600

    feat(iamzehan): Subscriber authentication controllers added

---
+++ Backend/API/src/controllers/subs.auth.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
+import { hashPassword, comparePassword } from "../utils/password.js";
+import { signAccessToken, signRefreshToken } from "../utils/jwt.js";
+
+export const register = async (req: Request, res: Response) => {
+  const { email, password, username, firstName, lastName } = req.body;
+  try{
+  const user = await prisma.subscriber.create({
+    data: {
+      email,
+      password: await hashPassword(password),
+      firstName,
+      lastName,
+      username
+    }
+  });
+  res.status(201).json({ id: user.id });
+} catch(err){
+  res.status(500).json({message: err});
+}
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await prisma.subscriber.findUnique({ where: { email  } });
+  if (!user || !(await comparePassword(password, user.password))) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const accessToken = signAccessToken(user.id);
+  const refreshToken = signRefreshToken(user.id);
+
+  req.session.userId = user.id;
+  req.session.refreshToken = refreshToken;
+
+  res.json({ accessToken });
+};
