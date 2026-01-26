562c27293429de9a4313623e56b0f69e3fc39571
Zehan <zehan.kareem@gmail.com>
2026-01-26 23:49:30 +0600

    feat(iamzehan): Subscriber authentication routes added

---
+++ Backend/API/src/routes/subs.auth.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { login, register } from "../controllers/subs.auth.controller.js";
+import { ensureGuest } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.post("/register", ensureGuest, register);
+router.post("/login", ensureGuest, login);
+
+export default router;
