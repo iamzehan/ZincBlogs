a82e31614bf9995331c6145bc36c34d3166063a2
Zehan <zehan.kareem@gmail.com>
2026-01-29 03:10:28 +0600

    dependency(iamzehan): added cookie parser and cors

--- Backend/API/src/app.ts
+++ Backend/API/src/app.ts
@@ -6,16 +6,24 @@ import morgan from "morgan";
 import routes from "./routes/index.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import { sessionMiddleware } from "./config/session.js";
-
+import { env } from "./config/env.js";
+import cookieParser from 'cookie-parser';
 // cron jobs
 import './jobs/index.js';
 
 const app = express();
 
+// CORS setup
+app.use(cors({
+  origin: env.CLIENT_URL,
+  credentials: true
+}));
+
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(sessionMiddleware);
-app.use(cors());
+
 app.use(helmet());
 app.use(compression());
 app.use(morgan("dev"));
