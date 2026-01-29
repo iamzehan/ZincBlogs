87d35573fbc7ceba75819165ff81d757061551a6
Zehan <zehan.kareem@gmail.com>
2026-01-29 22:02:00 +0600

    feat(iamzehan): Top bar shows logged in UserBadge. PageHeader shifts with Sidebar collapse behaviour.

--- Backend/CMS/src/components/Topbar.tsx
+++ Backend/CMS/src/components/Topbar.tsx
@@ -1,22 +1,62 @@
-import { useLocation } from "react-router-dom";
 import { links } from "../utils/links";
+import { useAuth, useIsMobile, useNav } from "../utils/hooks";
 import { useEffect, useState } from "react";
-
+import { Person } from "@mui/icons-material";
+import clsx from "clsx";
 export default function TopBar() {
-    const location = useLocation();
-    const currentLink = links.find(link=> link.path===location.pathname);
-    const [heading, setHeading] = useState(currentLink?.name);
+  return (
+    <>
+      <div
+        className="
+            flex justify-between items-center
+            w-full text-white text-2xl font-bold 
+            bg-zinc-900 fixed top-0 left-0 px-2 h-18 text-right z-80"
+      >
+        <PageHeader />
+        <UserBadge />
+      </div>
+    </>
+  );
+}
 
-    useEffect(()=> {
-        const currentLink = links.find(link=> link.path===location.pathname);
-        const headingHandler = async () => {
-            setHeading(currentLink?.name);
-        }
-        headingHandler();
-    }, [location])
-    return (
-        <>
-            <div className="w-full text-white text-2xl font-bold bg-zinc-900 fixed top-0 left-0 p-5 text-right z-80">{heading}</div>
-        </>
-    )
+// Page header 
+function PageHeader() {
+  const isMobile = useIsMobile();
+  const { collapse, location } = useNav();
+  const currentLink = links.find((link) => link.path === location.pathname);
+  const [heading, setHeading] = useState(currentLink?.name);
+
+  useEffect(() => {
+    const currentLink = links.find((link) => link.path === location.pathname);
+    const headingHandler = async () => {
+      setHeading(currentLink?.name);
+    };
+    headingHandler();
+  }, [location]);
+
+  return (
+    <div
+      className={clsx(
+        "text-xl transition-all duration-300",
+        { "ml-10": isMobile },
+        { "ml-[300px]": !collapse && !isMobile },
+        { "ml-15": collapse },
+      )}
+    >
+      {heading}
+    </div>
+  );
+}
+
+// Logged in user badge
+function UserBadge() {
+  const { user } = useAuth();
+  return (
+    <div className="text-sm flex items-center gap-2 px-4 py-2 ">
+      <span className="flex aspect-square p-1 rounded-full bg-white border">
+        <Person fontSize="small" className="text-zinc-500"/>
+      </span>
+      {user?.username}
+    </div>
+  );
 }
