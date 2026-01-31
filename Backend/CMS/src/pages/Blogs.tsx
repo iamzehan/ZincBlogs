e405fc8b535660ffdfa7bda2108b4ab29164f8a7
Zehan <zehan.kareem@gmail.com>
2026-02-01 00:23:32 +0600

    feat(iamzehan): Added blogs context provider, with useBlog hook

--- Backend/CMS/src/pages/Blogs.tsx
+++ Backend/CMS/src/pages/Blogs.tsx
@@ -1,16 +1,22 @@
 import BlogsTable from "../components/BlogsTable";
 import clsx from "clsx";
 import { useNav } from "../utils/hooks";
+import { BlogProvider } from "../utils/contexts.blog";
 
 export default function Page() {
-  const {collapse, isMobile} = useNav();
+  const { collapse, isMobile } = useNav();
   return (
-    <div className={clsx("m-4 mx-10 flex flex-col justify-center transition-all duration-300",
-        {"xl:w-[80vw] w-full lg:w-[90%] mx-4!": !collapse && !isMobile}, 
-        {"w-full lg:w-[90%] md:w-[90%] xl:w-[95vw]": collapse && !isMobile},
-        {"mx-2!": isMobile}
-        )}>
-      <BlogsTable />
-    </div>
+    <BlogProvider>
+      <div
+        className={clsx(
+          "m-4 mx-10 flex flex-col justify-center transition-all duration-300",
+          { "xl:w-[80vw] w-full lg:w-[90%] mx-4!": !collapse && !isMobile },
+          { "w-full lg:w-[90%] md:w-[90%] xl:w-[95vw]": collapse && !isMobile },
+          { "mx-2!": isMobile },
+        )}
+      >
+        <BlogsTable />
+      </div>
+    </BlogProvider>
   );
 }
