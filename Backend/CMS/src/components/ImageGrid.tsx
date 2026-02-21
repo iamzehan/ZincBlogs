0203867ca93aa3b1d5146f6e26190115bf8d6da0
Zehan <zehan.kareem@gmail.com>
2026-02-22 02:46:07 +0600

    fix(iamzehan): Grid layout fixed

--- Backend/CMS/src/components/ImageGrid.tsx
+++ Backend/CMS/src/components/ImageGrid.tsx
@@ -1,6 +1,6 @@
 export default function Grid ({children}: {children: React.ReactNode}){
     return (
-        <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-auto px-2">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4 w-screen px-2">
             {children}
         </div>
     );
