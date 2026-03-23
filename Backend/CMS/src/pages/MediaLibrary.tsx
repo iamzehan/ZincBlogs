904ecfefd820d818f3c1d2adbb2c1b444e1834bc
Zehan <zehan.kareem@gmail.com>
2026-03-23 13:08:01 +0600

    feat: Implement image grid with loading and error handling in MediaLibrary

--- Backend/CMS/src/pages/MediaLibrary.tsx
+++ Backend/CMS/src/pages/MediaLibrary.tsx
@@ -7,27 +7,74 @@ import {
   Fullscreen,
   Close,
   Link,
+  Error,
+  Done,
+  Image as ImageIcon
 } from "@mui/icons-material";
 import clsx from "clsx";
-import { useIsMobile } from "../utils/hooks";
+import { useAuth, useIsMobile, useMedia } from "../utils/hooks";
+import { MediaProvider } from "../utils/context.media";
+import { useQuery } from "@tanstack/react-query";
+import { getAllImages} from "../utils/requests.media";
+import { useDeleteMutation } from "../utils/query.hooks";
+
 
 export default function Page() {
   return (
-    <Grid>
-      <ImageCard />
-      <ImageCard />
-      <ImageCard />
-      <ImageCard />
-      <ImageCard />
-      <ImageCard />
-    </Grid>
+    <MediaProvider>
+      <ImageGrid />
+    </MediaProvider>
   );
+}
+interface ImageDataType {
+  id: string;
+  url: string;
+  public_id: string;
+}
+
+function ImageGrid() {
+  const { accessToken } = useAuth();
+  const { fetchWithAuth } = useMedia();
+  const { isPending, error, data } = useQuery({
+    queryKey: ["imageData"],
+    queryFn: () => fetchWithAuth(getAllImages, { accessToken }),
+    enabled: !!accessToken,
+  });
+  if (isPending) return "Loading Images...";
+  if (error)
+    return (
+      <div className="h-50 place-content-center text-xl text-red-500 overflow-x-auto rounded-xl border border-zinc-800 bg-zinc-950 transition-all duration-300 w-full">
+        Error loading Images <Error />
+      </div>
+    );
+  if (Array.isArray(data) && data?.length > 0)
+    return (
+      <Grid>
+        {Array.isArray(data) &&
+          data.map((img: ImageDataType) => (
+            <ImageCard key={img?.id} props={img} />
+          ))}
+      </Grid>
+    );
+  else {
+    return (
+      <div className="border-dashed border mx-2 rounded h-50 place-content-center">
+        No Images <ImageIcon fontSize="medium" className="text-red-400"/>
+      </div>
+    );
+  }
 }
 
 // Image cards
-function ImageCard() {
+function ImageCard({ props }: { props: ImageDataType }) {
+  // showMenu state is used for hover effect on the menu options
+  // hovering on an image shows the menu button
   const [showMenu, setMenu] = useState(false);
+
   const isMobile = useIsMobile();
+
+  // unpack props id: used for key, url: shows image
+  const { id, url } = props;
   return (
     <div
       onMouseEnter={() => setMenu(true)}
@@ -37,9 +84,13 @@ function ImageCard() {
       className="p-2 rounded flex flex-col gap-2 relative bg-zinc-900 shadow-md contain-content"
     >
       <Image
-        props={{ src: "./favicon.png", alt: "icon", className: "object-cover" }}
+        props={{
+          src: `${url}`,
+          alt: "icon",
+          className: "object-cover aspect-square",
+        }}
       />
-      
+
       {showMenu && (
         <span
           className={clsx(
@@ -48,19 +99,39 @@ function ImageCard() {
             { "bg-transparent": !showMenu },
           )}
         >
-          <ImageMenu />
+          <ImageMenu key={id} props={props} />
         </span>
       )}
       {showMenu && !isMobile && (
-        <p className="font-bold z-200 border border-blue-500 rounded-br rounded-bl border-t-0 absolute py-4 bottom-0 left-0 w-full bg-black/50">Image Name</p>
+        <p className="font-bold z-200 border border-blue-500 rounded-br rounded-bl border-t-0 absolute py-4 bottom-0 left-0 w-full bg-black/50">
+          Image Name
+        </p>
       )}
     </div>
   );
 }
 
 // Image Menu
-function ImageMenu() {
+function ImageMenu({ props }: { props: ImageDataType }) {
+  // Dropdown menu show hide true=show, false=hide
   const [showDropDown, setDropDown] = useState(false);
+
+  // Copy image URL
+  const [isCopied, setCopy] = useState<boolean>(false);
+  const handleCopy = async (text: string) => {
+    await navigator.clipboard.writeText(text);
+    setTimeout(() => setCopy(false), 800);
+    setCopy(true);
+  };
+
+  // mutation operation for image deletion
+  const deleteImageMutation = useDeleteMutation();
+  const handleDelete = async (public_id: string) => {
+    deleteImageMutation.mutate(public_id);
+  };
+  // unpack props - public_id is used to delete image and url is used to copy image and show full-screen
+  const { url, public_id } = props;
+
   return (
     <>
       <button
@@ -77,15 +148,21 @@ function ImageMenu() {
           { "scale-100": showDropDown },
         )}
       >
-        <div className="p-2 text-white hover:bg-green-500 hover:text-white flex items-center justify-between">
+        <div
+          onClick={() => handleCopy(url)}
+          className="p-2 text-white hover:bg-green-500 hover:text-white flex items-center justify-between"
+        >
           Copy URL
-          <Link fontSize="small" />
+          {!isCopied ? <Link fontSize="small" /> : <Done fontSize="small" />}
         </div>
         <div className="p-2 text-white hover:bg-zinc-500 border-t border-b border-zinc-300 hover:text-zinc-200 flex items-center justify-between">
           Full screen
           <Fullscreen fontSize="small" />
         </div>
-        <div className="p-2 text-red-500 hover:bg-red-500 hover:text-white flex items-center justify-between">
+        <div
+          onClick={() => handleDelete(public_id)}
+          className="p-2 text-red-500 hover:bg-red-500 hover:text-white flex items-center justify-between"
+        >
           Delete
           <DeleteOutline fontSize="small" />
         </div>
