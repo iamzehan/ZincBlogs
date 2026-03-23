182a90711ba46e4f2c498161f42dac0e408562a4
Zehan <zehan.kareem@gmail.com>
2026-03-23 13:08:58 +0600

    feat: Implement image fetching and deletion functions in requests.media.ts

---
+++ Backend/CMS/src/utils/requests.media.ts
@@ -0,0 +1,44 @@
+"use server";
+import { env } from "../config/env";
+
+interface FetchOptions {
+  accessToken?: string | null;
+  public_id?: string | null;
+}
+
+// the following function fetches all the images from cloud
+
+export const getAllImages = async (
+  options: FetchOptions,
+): Promise<Response> => {
+  const res = await fetch(`${env.VITE_BACKEND_URL}/api/files/images/all`, {
+    method: "GET",
+    credentials: "include",
+    cache: "no-store",
+    headers: {
+      Authorization: `Bearer ${options.accessToken}`,
+    },
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch Images");
+  }
+  return res;
+};
+
+// the following deletes an image from cloud and database
+
+export const deleteImage = async (options: FetchOptions): Promise<Response> => {
+  const res = await fetch(`${env.VITE_BACKEND_URL}/api/files/images/delete`, {
+    method: "DELETE",
+    credentials: "include",
+    cache: "no-store",
+    headers: {
+      Authorization: `Bearer ${options.accessToken}`,
+
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({ public_id: options.public_id }),
+  });
+  if (!res.ok) throw new Error("Failed to delete Image");
+  return res;
+};
