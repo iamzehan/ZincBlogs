0a21bbfbfce49b4606607f38b65853b1182c0198
Zehan <zehan.kareem@gmail.com>
2026-04-23 05:08:08 +0600

    refactor: streamline fetch requests by removing unnecessary line breaks and ensuring consistent formatting

--- Backend/CMS/src/utils/requests.blog.ts
+++ Backend/CMS/src/utils/requests.blog.ts
@@ -34,8 +34,7 @@ export const fetchAllBlogs = async (
 // the following function is responsible for publishing and unpublishing blogs
 export const publishBlog = async (options: FetchOptions): Promise<Response> => {
   const res = await fetch(
-    `
-    ${env.VITE_BACKEND_URL}/api/blog/posts/${options.id}/publish?status=${options.publish}`,
+    `${env.VITE_BACKEND_URL}/api/blog/posts/${options.id}/publish?status=${options.publish}`,
     {
       method: "PUT",
       credentials: "include",
@@ -52,16 +51,15 @@ export const publishBlog = async (options: FetchOptions): Promise<Response> => {
 // the following function will update a blog
 export const updateBlog = async (options: FetchOptions): Promise<Response> => {
   const res = await fetch(
-    `
-    ${env.VITE_BACKEND_URL}/api/blog/posts/${options.id}`,
+    `${env.VITE_BACKEND_URL}/api/blog/posts/${options.id}`,
     {
       method: "PUT",
       credentials: "include",
       headers: {
         Authorization: `Bearer ${options.accessToken}`,
-        "Content-Type": "application/json; charset=UTF-8"
+        "Content-Type": "application/json; charset=UTF-8",
       },
-      body: JSON.stringify(options.body)
+      body: JSON.stringify(options.body),
     },
   );
   if (!res.ok) throw new Error("Failed to publishBlog");
@@ -71,8 +69,7 @@ export const updateBlog = async (options: FetchOptions): Promise<Response> => {
 // the following function will get a blog
 export const getBlog = async (options: FetchOptions): Promise<Response> => {
   const res = await fetch(
-    `
-    ${env.VITE_BACKEND_URL}/api/blog/posts/${options.id}`,
+    `${env.VITE_BACKEND_URL}/api/blog/posts/${options.id}`,
     {
       method: "GET",
     },
@@ -83,37 +80,28 @@ export const getBlog = async (options: FetchOptions): Promise<Response> => {
 
 // the following function gets all the tags usable
 
-export const getTags = async (options:FetchOptions): Promise<Response> => {
-  const res = await fetch(
-    `
-    ${env.VITE_BACKEND_URL}/api/tags`,
-    {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        Authorization: `Bearer ${options.accessToken}`
-      }
+export const getTags = async (options: FetchOptions): Promise<Response> => {
+  const res = await fetch(`${env.VITE_BACKEND_URL}/api/tags`, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      Authorization: `Bearer ${options.accessToken}`,
     },
-  );
+  });
   if (!res.ok) throw new Error("Failed to get tags");
   return res;
 };
 
-
-// create a blog 
-export const createBlog = async (options: FetchOptions) : Promise<Response> => {
-  const res = await fetch(
-    `
-    ${env.VITE_BACKEND_URL}/api/blog/create`,
-    {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        Authorization: `Bearer ${options.accessToken}`,
-        "Content-Type": "application/json; charset=UTF-8"
-      },
-      body: JSON.stringify(options.body)
+// create a blog
+export const createBlog = async (options: FetchOptions): Promise<Response> => {
+  const res = await fetch(`${env.VITE_BACKEND_URL}/api/blog/create`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      Authorization: `Bearer ${options.accessToken}`,
+      "Content-Type": "application/json; charset=UTF-8",
     },
-  );
+    body: JSON.stringify(options.body),
+  });
   return res;
-}
+};
